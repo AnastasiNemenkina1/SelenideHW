d54750588644c92d32208584ca8ffc4b4e449f1d
Anastasia Nemenkina <gavrina.nastena@bk.ru>
2025-04-25 19:41:49 +0300

    Commit Message

--- src/test/java/ru/netology/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -3,44 +3,22 @@ package ru.netology.delivery;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    @Test
-    public void positiveTest() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        String Date = generateData(3, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(Date);
-        $("[data-test-id='name'] input").setValue("Анастасия Гаврина");
-        $("[data-test-id='phone'] input").setValue("+79102436802");
-        $("[data-test-id='agreement']").click();
-        $("button.button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + Date));
-    }
 
     @Test
-    public void shouldSubmitWithCitySelection() {
+    void shouldSubmitValidForm() {
         open("http://localhost:9999");
 
-        $("[data-test-id=city] input").setValue("Мо");
-        $$(".menu-item__control").findBy(Condition.text("Москва")).click();
-
-        String deliveryDate = generateDate(7);
-        $("[data-test-id=date] input").click();
-
-        if (!generateDate(3, "MM").equals(generateDate(7, "MM"))) {
-            $("[data-step='1']").click();
-        }
-
-        $$(".calendar__day").findBy(Condition.text(generateDate(7, "dd"))).click();
+        $("[data-test-id=city] input").setValue("Москва");
+        String deliveryDate = generateDate(3);
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(deliveryDate);
         $("[data-test-id=name] input").setValue("Анастасия Гаврина");
         $("[data-test-id=phone] input").setValue("+79102436802");
         $("[data-test-id=agreement]").click();
@@ -50,3 +28,9 @@ public class CardDeliveryTest {
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + deliveryDate));
     }
+
+    private String generateDate(int daysToAdd) {
+        return LocalDate.now().plusDays(daysToAdd)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+}
